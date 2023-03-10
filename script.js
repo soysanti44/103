29c1efd5eac066d0b5ad06fd52fab868378be6e6
soysanti44 <105685445+soysanti44@users.noreply.github.com>
2023-03-10 18:50:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-Webcam.attach("#camara")
-camara =document.getElementById("camara")
-Webcam.set({
-    width :350,
+Webcam.attach( '#camera' );
+
+
+camera = document.getElementById("camera");
+  Webcam.set({
+    width:350,
     height:300,
-    image_format:"png",
+    image_format : 'png',
     png_quality:90
-})
-function take_snapshot() {
-    Webcam.snap(function(data_uri){
-        document.getElementById("resultado").innerHTML = "<img id='selfie_img' src='"+data_uri+"' >"
-    })
+  });
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("resultado").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
 }
-classifier=ml5.imageClassifier("",modelLoaded)
-function modelLoaded() {
-    console.log("modelo cargado")
-}
-function check() {
-    img =document.getElementById("selfie_img")
-    classifier.classify(img,gotResult)
+
+
+  console.log('ml5 version:', ml5.version);
+ 
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/dctrbuamU/model.json',modelLoaded);
+
+
+  function modelLoaded() {
+    console.log('¡Modelo cargado!');
+  }
+     
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
 }
